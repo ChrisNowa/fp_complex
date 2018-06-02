d2b4fd13ef065d6a53d5e9e4dddd002949963499
dennisbenedikt <dennisbenedikt@denniss-macbook-air.fritz.box>
2018-06-02 16:10:55 +0200

    dao interface erstellt

---
+++ VocabularyTrainer/src/io/fp/vocabularyTrainer/dao/VocabularyTrainerDao.java
@@ -0,0 +1,30 @@
+package io.fp.vocabularyTrainer.dao;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.SortedMap;
+
+import io.fp.vocabularyTrainer.model.Word;
+
+public interface VocabularyTrainerDao {
+
+	
+ArrayList<Word> createWortBook() throws IOException;
+	
+ArrayList<Word> readWortBook() throws IOException;
+
+void updateWortBook(ArrayList<Word> book) throws IOException;
+
+void deleteWortBook() throws IOException;
+	
+
+SortedMap<String, Integer> createHighScore() throws IOException;
+
+SortedMap<String, Integer> readHighScore()  throws IOException;
+
+void updateHighScore(SortedMap<String, Integer> highScore) throws IOException;
+
+void deleteHighScore() throws IOException;
+
+
+}
