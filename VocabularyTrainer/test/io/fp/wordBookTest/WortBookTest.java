b1b21102d3b1aee776ca9cd45a6d3a8ea80e7878
dennisbenedikt <dennisbenedikt@denniss-macbook-air.fritz.box>
2018-06-10 14:44:51 +0200

    neue Tests eingefuegt

--- VocabularyTrainer/test/io/fp/wordBookTest/WortBookTest.java
+++ VocabularyTrainer/test/io/fp/wordBookTest/WortBookTest.java
@@ -28,6 +28,10 @@ public class WortBookTest {
 			modelTest.addNewWordPair("Affe", Language.GERMAN, "Monkey", Language.ENGLISH);
 			assertTrue(modelTest.getWordList().contains(modelTest.getWord1("Affe")));
 			assertTrue(modelTest.getWordList().contains(modelTest.getWord1("Monkey")));
+			assertEquals(Language.GERMAN, modelTest.getWord1("Affe").getLanguage());
+			assertEquals(Language.ENGLISH, modelTest.getWord1("Monkey").getLanguage());
+			assertEquals(modelTest.getWord1("Affe").getOrderNumbers(),modelTest.getWord1("Monkey").getOrderNumbers());
+			
 		} catch (WordException e) {
 			fail("Should not have thrown Exception");
 		}
@@ -53,6 +57,40 @@ public class WortBookTest {
 			fail("Should have thrown Exception");
 		} catch (WordException e) {
 
+		}
+
+	}
+
+	@Test
+	public void addWordTwiceFirst() {
+
+		try {
+			modelTest.addNewWordPair("Klar", Language.GERMAN, "Clear", Language.ENGLISH);
+			modelTest.addNewWordPair("Klar", Language.GERMAN, "Free", Language.ENGLISH);
+			assertTrue(modelTest.getWord1("Klar").getOrderNumbers().size() == 2);
+			assertTrue(modelTest.getWord1("Free").getOrderNumbers().size() == 1);
+			assertEquals(Integer.valueOf(4), modelTest.getWord1("Klar").getOrderNumbers().get(0));
+			assertEquals(Integer.valueOf(5), modelTest.getWord1("Klar").getOrderNumbers().get(1));
+			assertEquals(Integer.valueOf(4), modelTest.getWord1("Clear").getOrderNumbers().get(0));
+			assertEquals(Integer.valueOf(5), modelTest.getWord1("Free").getOrderNumbers().get(0));
+		} catch (WordException e) {
+			fail("Should have thrown Exception");
+		}
+	}
+
+	@Test
+	public void addWordTwiceSecond() {
+		try {
+			modelTest.addNewWordPair("Clear", Language.ENGLISH, "Klar", Language.GERMAN);
+			modelTest.addNewWordPair("Free", Language.ENGLISH, "Klar", Language.GERMAN);
+			assertTrue(modelTest.getWord1("Klar").getOrderNumbers().size() == 2);
+			assertTrue(modelTest.getWord1("Free").getOrderNumbers().size() == 1);
+			assertEquals(Integer.valueOf(4), modelTest.getWord1("Clear").getOrderNumbers().get(0));
+			assertEquals(Integer.valueOf(5), modelTest.getWord1("Free").getOrderNumbers().get(0));
+			assertEquals(Integer.valueOf(4), modelTest.getWord1("Klar").getOrderNumbers().get(0));
+			assertEquals(Integer.valueOf(5), modelTest.getWord1("Klar").getOrderNumbers().get(1));
+		} catch (WordException e) {
+			fail("Should have thrown Exception");
 		}
 
 	}
