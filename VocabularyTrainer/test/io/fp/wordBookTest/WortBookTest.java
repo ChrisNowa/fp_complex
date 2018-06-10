97583f50c2245aa942e1a27ac80d08e433366501
dennisbenedikt <dennisbenedikt@denniss-macbook-air.fritz.box>
2018-06-10 09:33:36 +0200

    Neue Test hinzugefuegt

--- VocabularyTrainer/test/io/fp/wordBookTest/WortBookTest.java
+++ VocabularyTrainer/test/io/fp/wordBookTest/WortBookTest.java
@@ -1,5 +1,6 @@
 package io.fp.wordBookTest;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -25,10 +26,33 @@ public class WortBookTest {
 
 		try {
 			modelTest.addNewWordPair("Affe", Language.GERMAN, "Monkey", Language.ENGLISH);
-            assertTrue(modelTest.getWordList().contains(modelTest.getWord1("Affe")));
-            assertTrue(modelTest.getWordList().contains(modelTest.getWord1("Monkey")));
+			assertTrue(modelTest.getWordList().contains(modelTest.getWord1("Affe")));
+			assertTrue(modelTest.getWordList().contains(modelTest.getWord1("Monkey")));
 		} catch (WordException e) {
 			fail("Should not have thrown Exception");
+		}
+
+	}
+
+	@Test
+	public void addEqualWordTest() {
+		try {
+			modelTest.addNewWordPair("Katze", Language.GERMAN, "Katze", Language.ENGLISH);
+			fail("Should have thrown Exception");
+		} catch (WordException e) {
+			assertTrue(!modelTest.getWordList().contains(modelTest.getWord1("Katze")));
+		}
+
+	}
+
+	@Test
+	public void addContainedWords() {
+		try {
+			modelTest.addNewWordPair("Affe", Language.GERMAN, "Monkey", Language.ENGLISH);
+			modelTest.addNewWordPair("Affe", Language.GERMAN, "Monkey", Language.ENGLISH);
+			fail("Should have thrown Exception");
+		} catch (WordException e) {
+
 		}
 
 	}
